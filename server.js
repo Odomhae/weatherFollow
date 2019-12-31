89548f6a528b4a17931b559fbdac246d8fa90c16
Odomhae <jihoon708@naver.com>
2020-01-01 00:07:14 +0900

    3rd

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
